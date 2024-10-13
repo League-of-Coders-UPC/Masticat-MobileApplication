29d8194c78f295ab1e12fc244ec06d86535742ff
ChristianZ <U202011688@upc.edu.pe>
2024-10-13 02:43:47 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/masticat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.masticat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:masticat/presentation/dashboard_screen.dart';
+import 'package:masticat/presentation/deviceManager_screen.dart';
+import 'package:masticat/presentation/login_screen.dart';
+import 'package:masticat/presentation/profile.dart';
+import 'package:masticat/presentation/addPett_screen.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Masticat App',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/dashboard': (context) => DashboardScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/addPet': (context) => AddPetDialog(),
+        '/deviceManager': (context) => DeviceManagerScreen(),
+      },
+    );
+  }
+}
