04c0508496b6c8cb4da8b1a956f17f5a48dd3276
ChristianZ <U202011688@upc.edu.pe>
2024-11-08 11:58:39 -0500

    Updating project

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,14 @@ import 'package:masticat/presentation/notification_screen.dart';
 import 'package:masticat/presentation/profile.dart';
 import 'package:masticat/presentation/addPett_screen.dart';
 import 'package:masticat/presentation/deviceManager_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
